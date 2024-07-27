933ab2c5cf1477beab1e6ae42ab18698a5f4b22f
Abhishek kumar <7011042290abhi@gmail.com>
2024-07-28 00:13:48 +0530

    Revised tree

--- tree.cpp
+++ tree.cpp
@@ -35,6 +35,66 @@ class Node
        return root;
     }
 
+    void levelOrder(Node* root)
+    {
+        queue<Node*> q;
+        q.push(root);
+        q.push(NULL);
+        while(!q.empty())
+        {
+            Node* front= q.front();
+            q.pop();
+            if(front==NULL)
+            {
+                cout<<endl;
+                if(!q.empty())
+                {
+                q.push(NULL);
+                }
+            }
+            else{
+            cout<<front->data<<" ";
+            
+            if(front->left!=NULL)
+            {
+                q.push(front->left);
+            }
+            if(front->right!=NULL)
+            {
+                q.push(front->right);
+            }
+            }
+        }
+    }
+
+    vector<vector<int>> levelOrderLEET(Node* root) {
+        queue<Node*> q;
+        vector<int> subans;
+        subans.push_back(root->data);
+        vector<vector<int>> ans;
+        q.push(root);
+        q.push(NULL);
+        while (q.size()>1) {
+            Node* front = q.front();
+            q.pop();
+            if (front == NULL) {
+                ans.push_back(subans);
+                subans.clear();
+                q.push(NULL);
+            } else {
+                subans.push_back(front->data);
+
+                if (front->left != NULL) {
+                    q.push(front->left);
+                }
+                if (front->right != NULL) {
+                    q.push(front->right);
+                }
+            }
+        }
+        return ans;
+    }
+
     void preOrder(Node* root)
     {
         if(root==NULL)
@@ -73,11 +133,20 @@ class Node
         postOrder(root->right);
         cout<<root->data<<" ";
     }
+    void printVectorOfVectors(const vector<vector<int>>& vec) {
+    for (const auto& subvec : vec) {
+        for (int val : subvec) {
+            cout << val << " ";
+        }
+        cout << endl; // Print a newline after each subvector
+    }
+}
 
     int main()
     {
         Node* root = createTree();
-        postOrder(root);
+        vector<vector<int>> answer=levelOrderLEET(root);
+        printVectorOfVectors(answer);
         return 0;
     }
 
